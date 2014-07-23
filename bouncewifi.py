cffee8dcf0736fa5c3a3917efc610f1d371717f7
awfki <github@davenoonan.com>
2014-07-23 17:36:14 -0400

    added some text output to bouncewifi

--- bouncewifi.py
+++ bouncewifi.py
@@ -2,7 +2,9 @@
 import os
 import time
 
+print "networksetup -setairportpower en0 off"
 os.system("networksetup -setairportpower en0 off")
 time.sleep( 5 )
+print "networksetup -setairportpower en0 on"
 os.system("networksetup -setairportpower en0 on")
 
